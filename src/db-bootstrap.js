e7e1c2c4e49026ebb905b6d0d4dd812d47e6fe0b
ShreyaBaliga2408 <baligashreyacc@gmail.com>
2023-10-04 17:37:14 -0400

    Commiting changes

--- src/db-bootstrap.js
+++ src/db-bootstrap.js
@@ -7,18 +7,14 @@ const { association } = require("../models/Association");
 
 console.log(process.env.DATABASE_URL);
 // Initialize Sequelize with your database connection details
-const sequelize = new Sequelize({
-  database:process.env.DB_DATABASE,
-  username:process.env.DB_USER,
-  password:process.env.DB_PASSWORD,
-  host:process.env.DB_HOST,
-  port:process.env.DB_PORT,
+// Initialize Sequelize with your database connection details
+const sequelize = new Sequelize(process.env.DATABASE_URL,
+  {
 
   
   dialect: 'postgres',
   logging: false,
 });
-
 
 const modelDefiners = [
   require("../models/Accounts"),
