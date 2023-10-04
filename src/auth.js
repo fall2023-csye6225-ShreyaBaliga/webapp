079a62c8fb0e7ac93a502b1cab94164732ff0e9b
ShreyaBaliga2408 <baligashreyacc@gmail.com>
2023-10-04 19:01:26 -0400

    Commiting

--- src/auth.js
+++ src/auth.js
@@ -4,16 +4,10 @@ const sequelize = require('./db-bootstrap');
 
 async function authenticateUser(email, password) {
   const user = await dbAccount(sequelize).findOne({ where: { email } });
-  console.log("acct model");
-  console.log(user);
  if(user)
  {
-  console.log(password + " " + user.dataValues.password);
   const hashedPassword = await bcrypt.hash(password, 10);
-  console.log(hashedPassword);
-  console.log(user.dataValues.password);
   const passwordMatch = await bcrypt.compare(password, user.dataValues.password);
-  console.log("ghjvhjv  " + passwordMatch);
   if (!passwordMatch) {
      const err = new Error("Bad Request");
      err.status = 401;
