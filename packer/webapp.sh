6d44b3ac8144dad6364ce9dc68a4941aea23eda2
ShreyaBaliga2408 <baligashreyacc@gmail.com>
2023-10-25 01:52:24 -0400

    test9

--- packer/webapp.sh
+++ packer/webapp.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get upgrade -y
 
-sudo apt-get update
-sudo apt-get install -y ca-certificates curl gnupg
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+set -e
 
-NODE_MAJOR=20
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y nodejs npm unzip
 
-sudo apt-get update
-sudo apt-get install nodejs -y
-
-sudo apt-get install unzip -y
 
 sudo unzip /home/admin/webapp.zip -d /home/admin/
 sudo rm -f /home/admin/webapp.zip
@@ -32,7 +24,8 @@ echo "Server setup completed!!"
 
 sudo sh -c "echo '[Unit]
 Description= My NPM Service
-After=network.target
+After=cloud-init.target
+Requires=cloud-init.target
 
 [Service]
 User=csye6225
@@ -44,7 +37,7 @@ Restart=always
 RestartSec=10
 
 [Install]
-WantedBy=multi-user.target' | sudo tee /etc/systemd/system/webapp.service"
+WantedBy=cloud-init.target' | sudo tee /etc/systemd/system/webapp.service"
 
 sudo systemctl daemon-reload
 sudo systemctl enable webapp
