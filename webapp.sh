82d11424b31de813ccba99270119a76db35ce840
ShreyaBaliga2408 <baligashreyacc@gmail.com>
2023-11-03 19:24:47 -0400

    TEST20

--- webapp.sh
+++ webapp.sh
@@ -39,8 +39,10 @@ echo "Server setup completed!!"
 # [Install]
 # WantedBy=cloud-init.target' | sudo tee /etc/systemd/system/webapp.service"
 sudo mv /tmp/webapp.service /etc/systemd/system/webapp.service
+sudo systemctl daemon-reload
 sudo systemctl enable webapp.service
 sudo systemctl start webapp.service
+
 
 
 # sudo systemctl daemon-reload
