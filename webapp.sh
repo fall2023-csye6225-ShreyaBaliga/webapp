443c1f34cec06b0ce62a2974370f74fc6ef20672
ShreyaBaliga2408 <baligashreyacc@gmail.com>
2023-11-06 22:13:04 -0500

    TEST33

--- webapp.sh
+++ webapp.sh
@@ -17,7 +17,7 @@ sudo cp /tmp/webapp.zip /opt/webapp.zip
 sudo groupadd csye6225
 sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
 sudo unzip /opt/webapp.zip -d /opt/csye6225/
-sudo rm -f /opt/csye6225/webapp.zip
+sudo rm -f /opt/webapp.zip
 sudo chown -R csye6225:csye6225 /opt/csye6225/webapp
 sudo chmod -R 770 /opt/csye6225/webapp
 
